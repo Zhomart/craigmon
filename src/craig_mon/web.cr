41580d9a7b469db0eb658a03fe8b79e68cd5f3ad
Zhomart Mukhamejanov <mzhomart@gmail.com>
2017-03-25 19:34:26 -0700

    added basic http auth

--- src/craig_mon/web.cr
+++ src/craig_mon/web.cr
@@ -1,5 +1,6 @@
 require "kemal"
 require "json"
+require "kemal-basic-auth"
 require "./web/*"
 
 module CraigMon
@@ -14,63 +15,80 @@ module CraigMon
 
     Kemal.config.add_filter_handler ServeBakedStatic.new
 
-    get "/api/searches" do |env|
-      env.response.content_type = "application/json"
-      { searches: all_searches() }.to_json
-    end
-
-    post "/api/searches" do |env|
-      env.response.content_type = "application/json"
-      search = Search.new
-      search.name = env.params.json["name"].to_s if env.params.json.has_key?("name")
-      search.url = env.params.json["url"].to_s if env.params.json.has_key?("url")
-      search.active = true
-      errors = Repo.insert(search).errors
-      if errors.empty?
-        { searches: all_searches() }.to_json
-      else
-        env.response.status_code = 400
-        { errors: errors }.to_json
-      end
-    end
-
-    patch "/api/searches/:id" do |env|
-      env.response.content_type = "application/json"
-      search = Repo.get(Search, env.params.url["id"]).as(Search)
-      search.active = !!env.params.json["active"] if env.params.json.has_key?("active")
-      search.name = env.params.json["name"].to_s if env.params.json.has_key?("name")
-      search.url = env.params.json["url"].to_s if env.params.json.has_key?("url")
-      searchChange = Repo.update(search)
-      if searchChange.errors.empty?
-        { search: searchChange.instance }.to_json
-      else
-        env.response.status_code = 400
-        message = searchChange.errors.map { |e| "#{e[:field]} #{e[:message]}" }
-        { errors: searchChange.errors, message: message }.to_json
-      end
-    end
-
-    get "/api/searches/:search_id/items" do |env|
-      env.response.content_type = "application/json"
-      search = Repo.get(Search, env.params.url["search_id"]).as(Search)
-      per_page = 20
-      total = Models::Item.total_count
-      pages = (total + per_page - 1) / per_page
-      page = env.params.query["page"]?
-      page = page ? page.to_i32 : 1
-      items = Models::Item.all_for(search.id, limit: per_page, offset: (page-1)*per_page)
-      { pages: pages, items: items, search: search }.to_json
-    end
-
-    get("/api/*" ) { |env| env.response.status_code = 404 }
-    get("/*") { FileStorage.get("/index.html").read }
-    get("/") { FileStorage.get("/index.html").read }
-
-    error 404 do
-      "404 - Page not found."
-    end
-
     def self.run
+      username, password = nil, nil
+
+      OptionParser.parse! do |parser|
+        parser.banner = "Usage: craigmon worker [arguments]"
+        parser.on("-u USER", "--user USER", "basic auth username") { |v| username = v }
+        parser.on("-p PASSWD", "--password PASSWD", "basic auth password") { |v| password = v }
+        parser.on("-h", "--help", "Show this help") {
+          puts parser
+          exit 0
+        }
+      end
+
+      if username.is_a?(String) && password.is_a?(String)
+        puts "Setting up basic auth, username=#{username}, password=***"
+        basic_auth username.as(String), password.as(String)
+      end
+
+      get "/api/searches" do |env|
+        env.response.content_type = "application/json"
+        { searches: all_searches() }.to_json
+      end
+
+      post "/api/searches" do |env|
+        env.response.content_type = "application/json"
+        search = Search.new
+        search.name = env.params.json["name"].to_s if env.params.json.has_key?("name")
+        search.url = env.params.json["url"].to_s if env.params.json.has_key?("url")
+        search.active = true
+        errors = Repo.insert(search).errors
+        if errors.empty?
+          { searches: all_searches() }.to_json
+        else
+          env.response.status_code = 400
+          { errors: errors }.to_json
+        end
+      end
+
+      patch "/api/searches/:id" do |env|
+        env.response.content_type = "application/json"
+        search = Repo.get(Search, env.params.url["id"]).as(Search)
+        search.active = !!env.params.json["active"] if env.params.json.has_key?("active")
+        search.name = env.params.json["name"].to_s if env.params.json.has_key?("name")
+        search.url = env.params.json["url"].to_s if env.params.json.has_key?("url")
+        searchChange = Repo.update(search)
+        if searchChange.errors.empty?
+          { search: searchChange.instance }.to_json
+        else
+          env.response.status_code = 400
+          message = searchChange.errors.map { |e| "#{e[:field]} #{e[:message]}" }
+          { errors: searchChange.errors, message: message }.to_json
+        end
+      end
+
+      get "/api/searches/:search_id/items" do |env|
+        env.response.content_type = "application/json"
+        search = Repo.get(Search, env.params.url["search_id"]).as(Search)
+        per_page = 20
+        total = Models::Item.total_count
+        pages = (total + per_page - 1) / per_page
+        page = env.params.query["page"]?
+        page = page ? page.to_i32 : 1
+        items = Models::Item.all_for(search.id, limit: per_page, offset: (page-1)*per_page)
+        { pages: pages, items: items, search: search }.to_json
+      end
+
+      get("/api/*" ) { |env| env.response.status_code = 404 }
+      get("/*") { FileStorage.get("/index.html").read }
+      get("/") { FileStorage.get("/index.html").read }
+
+      error 404 do
+        "404 - Page not found."
+      end
+
       Kemal.run
     end
 
